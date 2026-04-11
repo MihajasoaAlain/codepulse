32801e41f02597b06f714ea049d84cc9e4131061
Mihajasoa <mihajasoaalain85@gmail.com>
2026-04-11 22:43:25 +0300

    feat: implement user creation and repository management for GitHub OAuth

--- internal/features/users/routes/users_routes.go
+++ internal/features/users/routes/users_routes.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	"codepulse/internal/features/users/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UsersRoutes(r *gin.Engine) {
+	users := r.Group("/users")
+	users.POST("", handlers.AddUser)
+}
