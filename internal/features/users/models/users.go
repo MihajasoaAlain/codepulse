6669a54e5650eebae9bcd0d446bdb5c178a87f17
Mihajasoa <mihajasoaalain85@gmail.com>
2026-04-13 09:27:47 +0300

    add githubToken field to user

--- internal/features/users/models/users.go
+++ internal/features/users/models/users.go
@@ -7,11 +7,12 @@ import (
 )
 
 type User struct {
-	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Username  string             `json:"username,omitempty" bson:"username,omitempty"`
-	Email     string             `json:"email,omitempty" bson:"email,omitempty"`
-	CreatedAt time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
-	UpdatedAt time.Time          `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Username    string             `json:"username,omitempty" bson:"username,omitempty"`
+	Email       string             `json:"email,omitempty" bson:"email,omitempty"`
+	GithubToken string             `json:"githubToken,omitempty" bson:"githubToken,omitempty"`
+	CreatedAt   time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+	UpdatedAt   time.Time          `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
 }
 
 type CreateUserRequest struct {
